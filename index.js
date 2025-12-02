be4e614ef542c9802279f110153d22e2d06af0f0
sidneynyambok899-a11y <sidneynyambok899@gmail.com>
2025-12-02 14:03:34 +0300

    main

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-
+createLoginTracker(userInfo)
+let attemptCount = 0;
+//inner function to track login attempts
+const attemptlogin = (passwordattempt) => {
+  attemptCount++; //increment attempt count on each login attempt
+  if (attemptCount > 3) {
+    return 'Account locked due to too many failed login attempts.';//lock account after 3 failed attempts
+  }
+  if (passwordattempt === userInfo.password) {
+    attemptCount = 0; //reset attempt count on successful login
+    return `Login successful';//successful login message
+  } 
+    //Return failed login message with attempt count
+    return "attempt ${attemptCount}: Login failed
+    ";
+  }//return the inner function to track login attempts
+return attemptlogin;
 
 
 module.exports = {
